550405e444912c766af64c21caa900a3655ed879
cordis-dev <cordis.dev@gmail.com>
2016-10-19 14:05:53 +0300

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -10,6 +10,7 @@ namespace WebApplication9
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+    
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
