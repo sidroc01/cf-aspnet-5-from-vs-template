3138c0f732abcb38ac2cfda38f8e11d3baa46d35
cordis-dev <cordis.dev@gmail.com>
2016-11-22 15:50:11 +0200

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,7 +15,7 @@ namespace WebApplication9
         {
             AreaRegistration.RegisterAllAreas(); 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);;
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
         
