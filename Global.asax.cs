1f58c98ae37a2a89e8d91134d6098ef967170510
cordis-dev <cordis.dev@gmail.com>
2016-11-22 15:47:14 +0200

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -18,6 +18,7 @@ namespace WebApplication9
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        
     }
     
 }
