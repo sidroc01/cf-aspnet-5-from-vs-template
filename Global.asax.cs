836e7db1806a13d25cf85ad7c94b0b233e4d30a0
cordis-dev <cordis.dev@gmail.com>
2016-12-08 17:48:05 +0200

    new

--- Global.asax.cs
+++ Global.asax.cs
@@ -13,6 +13,7 @@ namespace WebApplication9
     
         protected void Application_Start()
         {
+        
             AreaRegistration.RegisterAllAreas(); 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
